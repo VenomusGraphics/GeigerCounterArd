c4a4eaecd7586926e07cce49f4d180c983df5bf2
Titan57 <115288658+Titan57@users.noreply.github.com>
2024-06-25 15:14:35 -0600

    Update Geiger_Counter_Arduino.ino

--- Geiger_Counter_Arduino.ino
+++ Geiger_Counter_Arduino.ino
@@ -4,24 +4,32 @@
 //  Voltages  outside  0  to  5V  will  destroy  your  Arduino!!!
 //  Don’t  wire  this  backwards!
 /////////////////////////////////////////////////////////
-//  define  a  variable  that  tells  which  analog  pin  we  will
-//       use
-int  AI0  =  0;         //AI0  stands  for  analog  input  zero
+//  define  a  variable  that  tells  which  analog  pin  we  will use
+
+int  AI0  =  1;         //AI0  stands  for  analog  input  zero
+int  AI1  =  2;
+
 //  define  a  variable  that  holds  our  Delta_v_min
+
 float  delta_v_min=0.0049;       //  volts  per  A2D  unit
+
 //  define  a  variable  for  our  A2D  version  of  our  signal 
+
 int  ADC_value  =  0;
-//  define  a  variable  for  our  voltage  version  of  our  signal 
+
+//  define  a  variable  for  our  voltage  version  of  our  signal
+
 float  voltage  =  0.0;
+
 ///////////////////////////////////////////////////////// 
 void  setup()  {
-//  put  your  setup  code  here,  to  run  once  when  your
-//  Arduino  starts  up:
+//  put  your  setup  code  here,  to  run  once  when  your  Arduino  starts  up:
 //
-//  Initiate  Serial  Communication,  so  we  can  see  the
-//  voltage  on  our  computer
+//  Initiate  Serial  Communication,  so  we  can  see the voltage  on  our  computer
+  
 Serial.begin(9600);         //9600  baud  rate
 }
+
 ///////////////////////////////////////////////////////// 
 void  loop()  {
 //  Read  in  the  voltage  in  A2D  units  form  the  serial  port 

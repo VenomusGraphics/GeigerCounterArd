d3d6bfed6c326015a4181e153cab540c12b96e61
jwinder500 <113638737+jwinder500@users.noreply.github.com>
2024-07-09 15:48:34 -0600

    gpt updates
    some things that chat gpt did

--- Finished_Geiger_Counter.ino
+++ Finished_Geiger_Counter.ino
@@ -1,11 +1,11 @@
 /*!
   @file geiger.ino
-  @brief    Detect CPM radiation intensity, the readings may have a large deviation at first, and the data tends to be stable after 3 times
-  @copyright   Copyright (c) 2010 DFRobot Co.Ltd (http://www.dfrobot.com)
-  @licence     The MIT License (MIT)
+  @brief Detect CPM radiation intensity, the readings may have a large deviation at first, and the data tends to be stable after 3 times
+  @copyright Copyright (c) 2010 DFRobot Co.Ltd (http://www.dfrobot.com)
+  @licence The MIT License (MIT)
   @author [fengli](li.feng@dfrobot.com)
-  @version  V1.0
-  @date  2021-9-17
+  @version V1.0
+  @date 2021-9-17
   @get from https://www.dfrobot.com
   @https://github.com/DFRobot/DFRobot_Geiger
 */
@@ -31,27 +31,24 @@
 
 // Edited by Tyler, Calvin, Isaac, and James
 
-#include <SD.h> //Library for SD card reading
-
+#include <SD.h> // Library for SD card reading
 #include <Wire.h>
 #include <SPI.h>
 #include <Adafruit_BME280.h>
-
-Adafruit_BME280 bme; // use I2C interface
-Adafruit_Sensor *bme_pressure = bme.getPressureSensor();
-
+#include <Adafruit_Sensor.h>
 #include <DFRobot_Geiger.h>
+
 #if defined ESP32
 #define detect_pin D3
 #else
 #define detect_pin 3
 #endif
-/*!
-   @brief Constructor
-   @param pin   External interrupt pin
-*/
-DFRobot_Geiger  geiger(detect_pin);
+
+DFRobot_Geiger geiger(detect_pin);
 const int SD_PIN = 10;
+
+Adafruit_BME280 bme; // I2C interface
+Adafruit_Sensor *bme_pressure = bme.getPressureSensor();
 
 int count = 0;
 int N = 15;
@@ -62,17 +59,16 @@ int red = 5;
 int yellow = 6;
 int green = 7;
 
-void setup()
-{
+void setup() {
   Serial.begin(9600);
-  //Start counting, enable external interrupt
+  // Start counting, enable external interrupt
   geiger.start();
   init_SD();
   logfile = open_next_logfile();
 
   Serial.println(F("BME280 Sensor event test"));
 
-  if (!bme.begin()) {
+  if (!bme.begin(0x76)) {  // 0x76 is the I2C address of the BME280 sensor
     Serial.println(F("Could not find a valid BME280 sensor, check wiring!"));
     while (1) delay(10);
   }
@@ -81,21 +77,14 @@ void setup()
 }
 
 void loop() {
-
   if (count < N) {
-    //Start counting, enable external interrupt
-    //geiger.start();
     delay(3000);
-    //Pause the count, turn off the external interrupt trigger, the CPM and radiation intensity values remain in the state before the pause
-    //geiger.pause();
-    //Get the current CPM, if it has been paused, the CPM is the last value before the pause
-    //Predict CPM by falling edge pulse within 3 seconds, the error is ±3CPM
+
     Serial.print("CPM:");
     Serial.println(geiger.getCPM());
     logfile.print("CPM:");
     logfile.println(geiger.getCPM());
     
-    //Get the current nSv/h, if it has been paused, nSv/h is the last value before the pause
     Serial.print("nSv/h:");
     Serial.println(geiger.getnSvh());
     logfile.print("nSv/h:");
@@ -105,29 +94,26 @@ void loop() {
       digitalWrite(red, HIGH);
       digitalWrite(yellow, LOW);
       digitalWrite(green, LOW);
-    }
-    else if (geiger.getnSvh() > 1020000) {
+    } else if (geiger.getnSvh() > 1020000) {
       digitalWrite(yellow, HIGH);
       digitalWrite(red, LOW);
       digitalWrite(green, LOW);
-    }
-    else if (geiger.getnSvh() > 10) {
+    } else if (geiger.getnSvh() > 10) {
       digitalWrite(green, HIGH);
       digitalWrite(red, LOW);
       digitalWrite(yellow, LOW);
-    }
-    else {
+    } else {
       digitalWrite(red, LOW);
       digitalWrite(yellow, LOW);
       digitalWrite(green, LOW);
     }
-    //Get the current μSv/h, if it has been paused, the μSv/h is the last value before the pause
+
     Serial.print("uSv/h:");
     Serial.println(geiger.getuSvh());
     logfile.print("uSv/h:");
     logfile.println(geiger.getuSvh());
   
-    sensors_event_t temp_event, pressure_event, humidity_event;
+    sensors_event_t pressure_event;
     bme_pressure->getEvent(&pressure_event);
   
     Serial.print(F("Pressure = "));
@@ -141,39 +127,33 @@ void loop() {
     logfile.println();
     delay(1000);
     count++;
-  }
-  else {
+  } else {
     logfile.close();
     count = 0;
     logfile = open_next_logfile();
   }
 }
 
-void init_SD()
-{
-  Serial.print("Initializing  SD  card...");
+void init_SD() {
+  Serial.print("Initializing SD card...");
   if (!SD.begin(SD_PIN)) {
-    Serial.println("  failed!");
-    while (1)
-      ;
+    Serial.println(" failed!");
+    while (1);
   }
-  Serial.println("  done!");
+  Serial.println(" done!");
 }
 
 File open_next_logfile() {
   char filename[24];
-  //  Create  folder  for  logs  if  it  doesn’t  already  exist 
-  if  (!SD.exists("/Geiger_Logs/")) {
+  if (!SD.exists("/Geiger_Logs/")) {
     SD.mkdir("/Geiger_Logs/");
   }
-  //  find  the  first  file  LOGxxx.TXT  that  doesn’t  exist, 
-  //  then  create,  open  and  use  that  file
   for (int logn = 0; logn < 1000; logn++) {
     sprintf(filename, "/Geiger_Logs/LOG%03d.TXT", logn);
     if (!SD.exists(filename)) {
-      Serial.print("Opened  \`SD:");
+      Serial.print("Opened `SD:");
       Serial.print(filename);
-      Serial.println("\`  for  logging.");
+      Serial.println("` for logging.");
       break;
     }
   }
